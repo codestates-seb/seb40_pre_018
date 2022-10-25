f7afe029cc7a27076f66bdac2530d93feb325b1a
seungmin2ee <tmdals2815@gmail.com>
2022-10-25 18:48:15 +0900

    common layout 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Common from "./components/Common";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>    
+      <Routes>
+        <Route path="/" element={<Common />}>
+
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
 import GlobalStyle from "./assets/style/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
