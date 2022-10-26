af067d503c48037804e09da7294a54d7e8f55db4
“casava840” <hen3403@gmail.com>
2022-10-26 18:02:07 +0900

    Feat:common-singleDto

---
+++ server/src/main/java/com/notfound/stackoverflowclone/dto/SingleResponseDto.java
@@ -0,0 +1,10 @@
+package com.notfound.stackoverflowclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+}
