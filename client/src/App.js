cea21cbe55659aa1a8bbf9e819ce71ad5608cd96
seungmin2ee <tmdals2815@gmail.com>
2022-10-27 04:21:44 +0900

    라우팅관련 주석 추가

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Common />}>
           <Route index element={<QuestionList />}></Route>
+          {/* Tags, Users, 질문 또는 답변 작성, 수정 페이지 라우팅은 여기 */}
         </Route>
+        {/* 로그인페이지 또는 회원가입페이지 라우팅은 여기 */}
       </Routes>
     </>
   );
