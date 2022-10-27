b70977fe2c629eee007b4c967b25b6f2eeee0a80
Qone2 <whrbdnjs33@naver.com>
2022-10-28 01:38:59 +0900

    Refactor: 애매한 메서드명 수정

--- server/src/main/java/com/notfound/stackoverflowclone/question/service/QuestionService.java
+++ server/src/main/java/com/notfound/stackoverflowclone/question/service/QuestionService.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -36,7 +34,7 @@ public class QuestionService {
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND));
     }
 
-    public Question findQuestion(Long questionId) {
+    public Question findClickedQuestion(Long questionId) {
         Question findQuestion = findVerifiedQuestion(questionId);
         findQuestion.setClicks(findQuestion.getClicks() + 1);
         return findQuestion;
