9166f81c2c17d947f8851af783d8ad09f78dc417
“casava840” <hen3403@gmail.com>
2022-10-28 21:17:14 +0900

    Feat: answerPost구현-AnswerService-save,createAnswer추가

--- server/src/main/java/com/notfound/stackoverflowclone/answer/service/AnswerService.java
+++ server/src/main/java/com/notfound/stackoverflowclone/answer/service/AnswerService.java
@@ -2,6 +2,8 @@ package com.notfound.stackoverflowclone.answer.service;
 
 import com.notfound.stackoverflowclone.answer.entity.Answer;
 import com.notfound.stackoverflowclone.answer.repository.AnswerRepository;
+import com.notfound.stackoverflowclone.user.entity.User;
+import com.notfound.stackoverflowclone.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AnswerService {
     private final AnswerRepository answerRepository;
+    private final UserService userService;
 
-    public Answer createAnswer(Answer answer){
-        return answerRepository.save(answer);
+    public Answer saveAnswer(Answer answer, Long userId){
+        User findUser = userService.findVerifiedUser(userId);
+        Answer madeAnswer = createAnswer(answer, findUser);
+        return answerRepository.save(madeAnswer);
+    }
+    private Answer createAnswer(Answer answer, User user) {
+        answer.setAuthor(user);
+        user.getAnswers().add(answer);
+        return answer;
     }
 }
