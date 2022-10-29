ac84777502d7cd0eb52959a5af334b1f45047aaa
“casava840” <hen3403@gmail.com>
2022-10-29 18:24:53 +0900

    Fix: AnswerService-createAnswer builder()수정

--- server/src/main/java/com/notfound/stackoverflowclone/answer/service/AnswerService.java
+++ server/src/main/java/com/notfound/stackoverflowclone/answer/service/AnswerService.java
@@ -25,17 +25,20 @@ public class AnswerService {
 
     public Answer saveAnswer(Answer answer, Long userId, Long questionId){
         User findUser = userService.findVerifiedUser(userId);
-        Question question = questionService.findVerifiedQuestion(questionId);
-        Answer madeAnswer = createAnswer(answer, findUser);
-        madeAnswer.builder()
-                .question(question)
-                .build();
+        Question findQuestion = questionService.findVerifiedQuestion(questionId);
+        Answer madeAnswer = createAnswer(answer, findQuestion, findUser);
         return answerRepository.save(madeAnswer);
     }
-    private Answer createAnswer(Answer answer, User user) {
+    private Answer createAnswer(Answer answer, Question question, User user) {
+        Answer createdAnswer = Answer.builder()
+                .content(answer.getContent()) //추후 answerDto.Post에 필드 추가시에 수정필요한부분
+                .author(user)
+                .question(question)
+                .build();
         answer.setAuthor(user);
-        user.getAnswers().add(answer);
-        return answer;
+        user.getAnswers().add(createdAnswer);
+        question.getAnswers().add(createdAnswer);
+        return createdAnswer;
     }
 
     public void deleteAnswer(Long answerId, Long userId){
