9589a8e7a2d3be67c83b113677e72c11994fc810
“casava840” <hen3403@gmail.com>
2022-10-29 14:18:54 +0900

    Feat: AnswerService-deleteAnswer추가

--- server/src/main/java/com/notfound/stackoverflowclone/answer/service/AnswerService.java
+++ server/src/main/java/com/notfound/stackoverflowclone/answer/service/AnswerService.java
@@ -2,10 +2,15 @@ package com.notfound.stackoverflowclone.answer.service;
 
 import com.notfound.stackoverflowclone.answer.entity.Answer;
 import com.notfound.stackoverflowclone.answer.repository.AnswerRepository;
+import com.notfound.stackoverflowclone.exception.BusinessLogicException;
+import com.notfound.stackoverflowclone.exception.ExceptionCode;
 import com.notfound.stackoverflowclone.user.entity.User;
 import com.notfound.stackoverflowclone.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +27,17 @@ public class AnswerService {
         answer.setAuthor(user);
         user.getAnswers().add(answer);
         return answer;
+    }
+
+    public void deleteAnswer(Long answerId, Long userId){
+        Optional<Answer> optionalAnswer = answerRepository.findById(answerId);
+        optionalAnswer.ifPresentOrElse(answer -> {
+                    if (!Objects.equals(answer.getAuthor().getUserId(), userId)) {
+                        throw new BusinessLogicException(ExceptionCode.USER_UNAUTHORIZED);
+                    }
+                    answerRepository.delete(answer);
+                },() -> {
+            return;
+                });
     }
 }
