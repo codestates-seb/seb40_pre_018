b5d304a7b8e0cb7e386793913bfa8d3020e2333f
seungmin2ee <tmdals2815@gmail.com>
2022-10-29 19:24:01 +0900

    Feat: 데이터 요청을 위한  useAxios 구현

---
+++ client/src/hooks/useAxios.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useAxios = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    axios(url)
+      .then((res) => {
+        setIsPending(false);
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setIsPending(true);
+        console.log('Error', err.message);
+      });
+  }, [url]);
+
+  return [data, isPending];
+};
+
+export default useAxios;
