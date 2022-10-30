e1d7e661f9523ab2b9e3076df215beeff6102be9
Jiwon <sjiwon9303@gmail.com>
2022-10-30 22:06:45 +0900

    Feat:회원가입 페이지 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import QuestionDetail from './pages/Questions/QuestionDetail';
 import AskQuestion from './pages/Questions/AskQuestion';
 import QuestionList from './pages/Questions/QuestionList';
 import EditQuestion from './pages/Questions/EditQuestion';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         </Route>
         <Route path="/ask" element={<AskQuestion />} />
         {/* 로그인페이지 또는 회원가입페이지 라우팅은 여기 */}
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
