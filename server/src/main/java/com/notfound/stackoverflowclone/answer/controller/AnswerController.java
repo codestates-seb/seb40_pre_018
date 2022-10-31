e198c4db9376d90ca55967d83681b6c4264cc9f7
“casava840” <hen3403@gmail.com>
2022-10-31 15:35:47 +0900

    Feat: AnswerVote기능-AnswerController-postAnswerVote추가

--- server/src/main/java/com/notfound/stackoverflowclone/answer/controller/AnswerController.java
+++ server/src/main/java/com/notfound/stackoverflowclone/answer/controller/AnswerController.java
@@ -4,6 +4,9 @@ import com.notfound.stackoverflowclone.answer.dto.AnswerDto;
 import com.notfound.stackoverflowclone.answer.entity.Answer;
 import com.notfound.stackoverflowclone.answer.mapper.AnswerMapper;
 import com.notfound.stackoverflowclone.answer.service.AnswerService;
+import com.notfound.stackoverflowclone.vote.dto.VoteDto;
+import com.notfound.stackoverflowclone.vote.entity.Vote;
+import com.notfound.stackoverflowclone.vote.mapper.VoteMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -19,6 +22,7 @@ import javax.validation.constraints.Positive;
 public class AnswerController {
     private final AnswerService answerService;
     private final AnswerMapper mapper;
+    private final VoteMapper voteMapper;
 
     @PostMapping("/questions/{question-id}/answers")
     @ResponseStatus(HttpStatus.CREATED)
@@ -35,5 +39,16 @@ public class AnswerController {
     void deleteAnswer(@PathVariable(name = "answer-id") Long answerId,
                       @RequestHeader(name = "Authorization") Long userId){
         answerService.deleteAnswer(answerId,userId);
+    }
+
+    @PostMapping("/questions/{question-id}/answers/{answer-id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public VoteDto.Response postAnswerVote(
+            @RequestHeader(name = "Authorization") Long userId,
+            @PathVariable("answer-id") Long answerId,
+            @PathVariable("question-id") Long questionId,
+            @Valid @RequestBody VoteDto.Patch patchDto){
+        Vote vote = voteMapper.patchDtoToEntity(patchDto);
+        return voteMapper.entityToResponseDto(answerService.saveVote(answerId,userId,questionId,vote.getAmount()));
     }
 }
