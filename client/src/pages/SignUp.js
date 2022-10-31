862f547e63240c4cc25d1356287d5810c1bad6c7
Jiwon <sjiwon9303@gmail.com>
2022-10-31 13:30:30 +0900

    디자인 적용, axios Post요청으로 회원가입 기능구현

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,67 +1,164 @@
 /* eslint-disable no-unused-vars */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { CommonButton } from '../components/Buttons';
 
+// 회원가입 페이지 컨테이너
 const SinUpPage = styled.section`
   width: 100%;
+  height: 100vh;
   padding-top: 50px;
   display: flex;
+  align-items: center;
   justify-content: center;
   background-color: var(--black-025);
 
-  .signup-block {
+  .signup-left-container {
+    margin-right: 40px;
+
+    > h1 {
+      font-weight: 400;
+      color: var(--black-800);
+    }
+
+    li {
+      margin-bottom: 24px;
+      font-size: 15px;
+      color: var(--black-800);
+    }
+
+    .signup-term {
+      font-size: small;
+      > div {
+        margin-bottom: 5px;
+        color: var(--black-500);
+      }
+      a {
+        color: var(--blue-600);
+      }
+    }
   }
+  /* signUp 전체 박스 */
+  .signup-right-contianer {
+    width: 310px;
+  }
+  /* 소셜가입 버튼 박스 */
   .social-btn-container {
     display: flex;
     flex-direction: column;
   }
+  .signup-footer {
+    text-align: center;
+    font-size: 13px;
+    color: var(--black-700);
+    margin-top: 40px;
+    a {
+      margin-left: 6px;
+      color: var(--blue-600);
+    }
+  }
 `;
-
+// 회원가입 폼 제출 전체 박스
 const SignUpFormContainer = styled.div`
   width: 100%;
   padding-top: 50px;
   display: flex;
+  box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.06), 0 2px 6px hsla(0, 0%, 0%, 0.06),
+    0 3px 8px hsla(0, 0%, 0%, 0.09);
   justify-content: center;
   flex-direction: column;
+  background-color: white;
+  padding: 24px;
   div {
     display: flex;
     flex-direction: column;
+    font-weight: 500;
   }
-  input {
-    width: 300px;
+  .password-limit {
+    font-size: small;
+    font-weight: 400;
+    color: var(--black-500);
+    margin: -7px 0px 20px 3px;
+  }
+  .signup-policy {
+    font-size: small;
+    color: var(--black-500);
+    margin: 30px 3px 10px 3px;
+    a {
+      color: var(--blue-600);
+    }
   }
 `;
 
-const SignUpInputForm = styled.input``;
+const SignUpInputForm = styled.input`
+  width: auto;
+  height: 16px;
+  padding: 1.2em;
+  margin-top: 5px;
+  margin-bottom: 16px;
+  border-radius: 3px;
+  border: 1px solid var(--black-100);
+`;
 
 const SignUp = () => {
+  const [displayName, setDisplayName] = useState('aaa');
+  const [email, setEmail] = useState('a@aa');
+  const [password, setPassword] = useState('aaaa');
+  const [loading, setLoading] = useState(false);
+  const [userAccount, setUserAccount] = useState('');
+
+  const signUpSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios
+        .post(`http://15.165.244.155:8080/users/`, {
+          email,
+          displayName,
+          password,
+        })
+        .then((response) => console.log('회원가입 완료 : ' + response.data));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <SinUpPage>
-      <div className="signup-guide-container">
+      {/* 좌측 컨테이너 */}
+      <div className="signup-left-container">
         <h1>Join the Stack Overflow community</h1>
-        <div>
-          <div>Get unstuck — ask a question</div>
-          <div>Unlock new privileges like voting and commenting</div>
-          <div>Save your favorite tags, filters, and jobs</div>
-          <div>Earn reputation and badges</div>
-        </div>
-        <div>
-          <p>Collaborate and share knowledge with a private group for FREE.</p>
+        <ul>
+          <li>Get unstuck — ask a question</li>
+          <li>Unlock new privileges like voting and commenting</li>
+          <li>Save your favorite tags, filters, and jobs</li>
+          <li>Earn reputation and badges</li>
+        </ul>
+        <div className="signup-term">
+          <div>
+            Collaborate and share knowledge with a private group for FREE.
+          </div>
           <a
             target="_blank"
-            href="https://stackoverflow.co/teams/?utm_source=so-owned&utm_medium=product&utm_campaign=free-50&utm_content=public-sign-up"
             rel="noreferrer"
+            href="https://stackoverflow.co/teams/?utm_source=so-owned&utm_medium=product&utm_campaign=free-50&utm_content=public-sign-up"
           >
             Get Stack Overflow for Teams free for up to 50 users.
           </a>
         </div>
       </div>
-      <form className="signup-block">
+      {/* 우측 컨테이너 */}
+      <form className="signup-right-contianer">
+        {/* 소셜 회원가입 미정
         <div className="social-btn-container">
           <CommonButton>Sign up with google</CommonButton>
           <CommonButton>Sign up with GitHub</CommonButton>
-          <CommonButton>Sign up with Facebook</CommonButton>
-        </div>
+          <CommonButton onClick={signUpSubmit}>
+            Sign up with Facebook
+          </CommonButton>
+        </div> */}
         <SignUpFormContainer>
           <div>
             Display name
@@ -74,7 +171,7 @@ const SignUp = () => {
           <div className="password-box">
             Password
             <SignUpInputForm></SignUpInputForm>
-            <div>
+            <div className="password-limit">
               Passwords must contain at least eight characters, including at
               least 1 letter and 1 number.
             </div>
@@ -91,7 +188,48 @@ const SignUp = () => {
           >
             Sign up
           </CommonButton>
+          <span className="signup-policy">
+            By clicking “Sign up”, you agree to our &nbsp;
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://stackoverflow.com/legal/terms-of-service/public"
+            >
+              terms of service, &nbsp;
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://stackoverflow.com/legal/privacy-policy"
+            >
+              privacy policy
+            </a>
+            &nbsp; and
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://stackoverflow.com/legal/cookie-policy"
+            >
+              &nbsp; cookie policy
+            </a>
+          </span>
         </SignUpFormContainer>
+        <div className="signup-footer">
+          <div>
+            Already have an account?
+            <Link to="/login">Log in</Link>
+          </div>
+          <div>
+            Are you an employer?
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://talent.stackoverflow.com/users/login"
+            >
+              Sign up on Talent
+            </a>
+          </div>
+        </div>
       </form>
     </SinUpPage>
   );
