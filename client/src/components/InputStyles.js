1d10187684f517bceed15c7f3f520d444950e7f7
seungmin2ee <tmdals2815@gmail.com>
2022-10-31 16:25:30 +0900

    Design: input 스타일 추가

--- client/src/components/InputStyles.js
+++ client/src/components/InputStyles.js
@@ -7,13 +7,14 @@ const commonInput = css`
   background-color: #fff;
   color: hsl(210, 8%, 5%);
   font-size: 13px;
-  border: 1px solid var(--black-200);
+  border: 1px solid ${(props) => props.border || 'var(--black-200)'};
   border-radius: 3px;
   outline: none;
 
   &:focus {
-    box-shadow: 0px 0px 0px 4px var(--powder-200);
-    border-color: var(--blue-600);
+    box-shadow: 0px 0px 0px 4px
+      ${(props) => props.shadow || 'var(--powder-200)'};
+    border-color: ${(props) => props.focusBorder || 'var(--blue-600)'};
   }
 `;
 
