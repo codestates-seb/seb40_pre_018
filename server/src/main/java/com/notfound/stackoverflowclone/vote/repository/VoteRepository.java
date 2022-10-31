6d036364a2f3aa0045eb64c3073db440c97dec24
“casava840” <hen3403@gmail.com>
2022-10-31 15:42:53 +0900

    Refactor: AnswerVoteRepository->VoteRepository이름변경

--- server/src/main/java/com/notfound/stackoverflowclone/vote/repository/VoteRepository.java
+++ server/src/main/java/com/notfound/stackoverflowclone/vote/repository/VoteRepository.java
@@ -3,5 +3,5 @@ package com.notfound.stackoverflowclone.vote.repository;
 import com.notfound.stackoverflowclone.vote.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerVoteRepository extends JpaRepository<Vote, Long> {
+public interface VoteRepository extends JpaRepository<Vote, Long> {
 }
