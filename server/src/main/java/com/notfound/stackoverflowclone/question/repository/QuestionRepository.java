6f3f8c7103f90f9d7f424cc0fb5c156a84082df3
Qone2 <whrbdnjs33@naver.com>
2022-11-03 23:19:13 +0900

    Feat: 질문 레포지토리 검색 수정

--- server/src/main/java/com/notfound/stackoverflowclone/question/repository/QuestionRepository.java
+++ server/src/main/java/com/notfound/stackoverflowclone/question/repository/QuestionRepository.java
@@ -1,10 +1,10 @@
 package com.notfound.stackoverflowclone.question.repository;
 
 import com.notfound.stackoverflowclone.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findAllByTitleLikeOrContentLike(String title, String content);
+    Page<Question> findAllByTitleLikeOrContentLike(String title, String content, Pageable pageable);
 }
