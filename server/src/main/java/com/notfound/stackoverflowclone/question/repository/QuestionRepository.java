be7457b94f1879a7e1f10fad59a165258c8a6ff5
Qone2 <whrbdnjs33@naver.com>
2022-11-03 22:43:48 +0900

    Feat: 질문 레포지토리 검색 작성

--- server/src/main/java/com/notfound/stackoverflowclone/question/repository/QuestionRepository.java
+++ server/src/main/java/com/notfound/stackoverflowclone/question/repository/QuestionRepository.java
@@ -3,5 +3,8 @@ package com.notfound.stackoverflowclone.question.repository;
 import com.notfound.stackoverflowclone.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByTitleLikeOrContentLike(String title, String content);
 }
