7399e29f6c515c6948c7eade04c0f597ddc29824
“casava840” <hen3403@gmail.com>
2022-11-03 22:22:39 +0900

    Refactor: voter_id로 변경

--- server/src/main/java/com/notfound/stackoverflowclone/vote/entity/Vote.java
+++ server/src/main/java/com/notfound/stackoverflowclone/vote/entity/Vote.java
@@ -16,8 +16,8 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Builder
 @Table(uniqueConstraints = {
-        @UniqueConstraint(name = "CantVoteAnswerTwice", columnNames = {"user_id", "answer_id"}),
-        @UniqueConstraint(name = "CantVoteQuestionTwice", columnNames = {"user_id", "question_id"})
+        @UniqueConstraint(name = "CantVoteAnswerTwice", columnNames = {"voter_id", "answer_id"}),
+        @UniqueConstraint(name = "CantVoteQuestionTwice", columnNames = {"voter_id", "question_id"})
 })
 public class Vote extends AuditingEntity {
     @Id
@@ -25,7 +25,7 @@ public class Vote extends AuditingEntity {
     private Long voteId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "voter_id")
     private User voter;
 
     @ManyToOne
