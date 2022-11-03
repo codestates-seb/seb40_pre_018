147a893a428556efce74e5911688d94430e19625
“casava840” <hen3403@gmail.com>
2022-11-03 23:39:50 +0900

    Refactor: VoteController-return타입을 바로 Dto로 리턴하도록 변경.

--- server/src/main/java/com/notfound/stackoverflowclone/vote/controller/VoteController.java
+++ server/src/main/java/com/notfound/stackoverflowclone/vote/controller/VoteController.java
@@ -2,7 +2,6 @@ package com.notfound.stackoverflowclone.vote.controller;
 
 import com.notfound.stackoverflowclone.auth.jwt.JwtTokenizer;
 import com.notfound.stackoverflowclone.vote.dto.VoteDto;
-import com.notfound.stackoverflowclone.vote.mapper.VoteMapper;
 import com.notfound.stackoverflowclone.vote.service.VoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class VoteController {
     private final VoteService voteService;
-    private final VoteMapper voteMapper;
     private final JwtTokenizer jwtTokenizer;
 
     @PostMapping("/answers/{answer-id}/upvotes")
@@ -23,13 +21,13 @@ public class VoteController {
     public VoteDto.Response postAnswerUpVote(
             @RequestHeader(name = "Authorization") String token,
             @PathVariable("answer-id") Long answerId){
-        return voteMapper.entityToResponseDto(voteService.saveVote(answerId,jwtTokenizer.getUserId(token),1));
+        return voteService.saveVote(answerId,jwtTokenizer.getUserId(token),1);
     }
     @PostMapping("/answers/{answer-id}/downvotes")
     @ResponseStatus(HttpStatus.CREATED)
     public VoteDto.Response postAnswerDownVote(
             @RequestHeader(name = "Authorization") String token,
             @PathVariable("answer-id") Long answerId){
-        return voteMapper.entityToResponseDto(voteService.saveVote(answerId, jwtTokenizer.getUserId(token),-1));
+        return voteService.saveVote(answerId, jwtTokenizer.getUserId(token),-1);
     }
 }
