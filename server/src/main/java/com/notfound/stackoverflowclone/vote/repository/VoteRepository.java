b5566146fcf34e934de5791330b65a2acb6946ee
“casava840” <hen3403@gmail.com>
2022-11-03 22:22:06 +0900

    Refactor: VoteRepository-findAllByVoterAndAnswer추가

--- server/src/main/java/com/notfound/stackoverflowclone/vote/repository/VoteRepository.java
+++ server/src/main/java/com/notfound/stackoverflowclone/vote/repository/VoteRepository.java
@@ -1,7 +1,12 @@
 package com.notfound.stackoverflowclone.vote.repository;
 
+import com.notfound.stackoverflowclone.answer.entity.Answer;
+import com.notfound.stackoverflowclone.user.entity.User;
 import com.notfound.stackoverflowclone.vote.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    List<Vote> findAllByVoterAndAnswer(User user, Answer answer);
 }
