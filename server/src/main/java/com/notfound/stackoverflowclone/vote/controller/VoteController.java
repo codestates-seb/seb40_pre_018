958b28814c1f990dff24a25d0331b9e58daa40d6
“casava840” <hen3403@gmail.com>
2022-11-03 22:21:02 +0900

    Refactor: VoteController token 적용

--- server/src/main/java/com/notfound/stackoverflowclone/vote/controller/VoteController.java
+++ server/src/main/java/com/notfound/stackoverflowclone/vote/controller/VoteController.java
@@ -1,5 +1,6 @@
 package com.notfound.stackoverflowclone.vote.controller;
 
+import com.notfound.stackoverflowclone.auth.jwt.JwtTokenizer;
 import com.notfound.stackoverflowclone.vote.dto.VoteDto;
 import com.notfound.stackoverflowclone.vote.mapper.VoteMapper;
 import com.notfound.stackoverflowclone.vote.service.VoteService;
@@ -15,19 +16,20 @@ import org.springframework.web.bind.annotation.*;
 public class VoteController {
     private final VoteService voteService;
     private final VoteMapper voteMapper;
+    private final JwtTokenizer jwtTokenizer;
 
     @PostMapping("/answers/{answer-id}/upvotes")
     @ResponseStatus(HttpStatus.CREATED)
     public VoteDto.Response postAnswerUpVote(
-            @RequestHeader(name = "Authorization") Long userId,
+            @RequestHeader(name = "Authorization") String token,
             @PathVariable("answer-id") Long answerId){
-        return voteMapper.entityToResponseDto(voteService.saveVote(answerId,userId,1));
+        return voteMapper.entityToResponseDto(voteService.saveVote(answerId,jwtTokenizer.getUserId(token),1));
     }
     @PostMapping("/answers/{answer-id}/downvotes")
     @ResponseStatus(HttpStatus.CREATED)
     public VoteDto.Response postAnswerDownVote(
-            @RequestHeader(name = "Authorization") Long userId,
+            @RequestHeader(name = "Authorization") String token,
             @PathVariable("answer-id") Long answerId){
-        return voteMapper.entityToResponseDto(voteService.saveVote(answerId,userId,-1));
+        return voteMapper.entityToResponseDto(voteService.saveVote(answerId, jwtTokenizer.getUserId(token),-1));
     }
 }
